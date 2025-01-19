35ea8cd4b2b9b200c1548a983a49217792aa2fe6
kwak-dong-su <srs3576@gmail.com>
2025-01-20 04:13:37 +0900

    fix api param

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 from fastapi import FastAPI
 
-from api.translation.request.request import Message
 from api.translation.service import service as translation
 
 app = FastAPI()
 
 
 @app.get("/translate")
-async def translate(message: Message):
-    return translation.translate(message.content)
+async def translate(message: str):
+    return translation.translate(message)
